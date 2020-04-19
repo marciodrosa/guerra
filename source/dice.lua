c91eb8d3b8493a63bad3b84fc2d2dfe2f189fbd7
Márcio Rosa <marciodrosa@gmail.com>
2020-04-19 14:32:27 -0300

    New functions to compare dices values.

--- source/dice.lua
+++ source/dice.lua
@@ -1,10 +1,32 @@
 -- Utility table that simulates a dice.
 
+local function sort_dices(dice_1, dice_2, dice_3)
+	local list = {}
+	if dice_1 ~= nil then table.insert(list, dice_1) end
+	if dice_2 ~= nil then table.insert(list, dice_2) end
+	if dice_3 ~= nil then table.insert(list, dice_3) end
+	table.sort(list, function(d1, d2) return d1.value > d2.value end)
+	return list[1], list[2], list[3]
+end
+
+local function compare_dices_pair(attack_dice, defence_dice)
+	if attack_dice ~= nil and defence_dice ~= nil then
+		if attack_dice.value > defence_dice.value then
+			return 1, 0
+		else
+			return 0, 1
+		end
+	else
+		return 0, 0
+	end
+end
+
 return {
 
 	-- Returns a new table tha represents a dice. Contains a function named "roll" that changes the value of the field
 	-- "value" randomly. The dice number of sides can be configured with the "sides" field (default is 6).
-	new = function()
+	-- Receives an optional initial_value. If nil, a random initial value is setted.
+	new = function(initial_value)
 		local dice = {
 			sides = 6,
 			value = 1
@@ -20,8 +42,28 @@ return {
 		end
 
 		dice.roll = roll
-		dice.value = generate_random_dice_value()
+		if initial_value ~= nil then
+			dice.value = initial_value
+		else
+			dice.value = generate_random_dice_value()
+		end
 		return dice
-	end
+	end,
 
+	-- Receives three dices and returns the same three values, but sorted from the bigger value to the smaller one.
+	sort = sort_dices,
+
+	-- Compares the value of the dices. Each dice is optional, may be nil. Defense dices win in case of a draw.
+	-- Returns a table with two numeric fields: "attack_victories" and "defense_victories".
+	compare = function(attack_dice_1, attack_dice_2, attack_dice_3, defence_dice_1, defence_dice_2, defence_dice_3)
+		attack_dice_1, attack_dice_2, attack_dice_3 = sort_dices(attack_dice_1, attack_dice_2, attack_dice_3)
+		defence_dice_1, defence_dice_2, defence_dice_3 = sort_dices(defence_dice_1, defence_dice_2, defence_dice_3)
+		local attack_victories_pair_1, defence_victories_pair_1 = compare_dices_pair(attack_dice_1, defence_dice_1)
+		local attack_victories_pair_2, defence_victories_pair_2 = compare_dices_pair(attack_dice_2, defence_dice_2)
+		local attack_victories_pair_3, defence_victories_pair_3 = compare_dices_pair(attack_dice_3, defence_dice_3)
+		return {
+			attack_victories = attack_victories_pair_1 + attack_victories_pair_2 + attack_victories_pair_3,
+			defense_victories = defence_victories_pair_1 + defence_victories_pair_2 + defence_victories_pair_3
+		}
+	end
 }
--- test/dice_test.lua
+++ test/dice_test.lua
@@ -38,3 +38,177 @@ function test_dice.test_should_return_the_value_after_call_roll_function()
 	-- then:
 	lu.assertEquals(returned_value, d.value, "The roll function should return the same value of the dice.")
 end
+
+function test_dice.test_should_sort_dices_from_bigger_value_to_smaller()
+	-- given:
+	local d1 = dice.new()
+	local d2 = dice.new()
+	local d3 = dice.new()
+	d1.value = 3
+	d2.value = 6
+	d3.value = 4
+
+	-- when:
+	d1, d2, d3 = dice.sort(d1, d2, d3)
+
+	-- then:
+	lu.assertEquals(d1.value, 6)
+	lu.assertEquals(d2.value, 4)
+	lu.assertEquals(d3.value, 3)
+end
+
+function test_dice.test_should_sort_dices_from_bigger_value_to_smaller_including_dices_with_equal_values()
+	-- given:
+	local d1 = dice.new()
+	local d2 = dice.new()
+	local d3 = dice.new()
+	d1.value = 3
+	d2.value = 6
+	d3.value = 3
+
+	-- when:
+	d1, d2, d3 = dice.sort(d1, d2, d3)
+
+	-- then:
+	lu.assertEquals(d1.value, 6)
+	lu.assertEquals(d2.value, 3)
+	lu.assertEquals(d3.value, 3)
+end
+
+function test_dice.test_should_sort_two_dices_from_bigger_value_to_smaller()
+	-- given:
+	local d1 = dice.new()
+	local d2 = dice.new()
+	d1.value = 3
+	d2.value = 6
+
+	-- when:
+	d1, d2 = dice.sort(d1, d2)
+
+	-- then:
+	lu.assertEquals(d1.value, 6)
+	lu.assertEquals(d2.value, 3)
+end
+
+function test_dice.test_should_sort_only_one_dice()
+	-- given:
+	local d1 = dice.new()
+	d1.value = 3
+
+	-- when:
+	d1 = dice.sort(d1)
+
+	-- then:
+	lu.assertEquals(d1.value, 3)
+end
+
+function test_dice.test_should_compare_dices()
+	-- given
+	local attack_dice_1 = dice.new(3)
+	local attack_dice_2 = dice.new(6)
+	local attack_dice_3 = dice.new(1)
+	local defense_dice_1 = dice.new(5)
+	local defense_dice_2 = dice.new(4)
+	local defense_dice_3 = dice.new(2)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, attack_dice_2, attack_dice_3, defense_dice_1, defense_dice_2, defense_dice_3)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 1)
+	lu.assertEquals(result.defense_victories, 2)
+end
+
+function test_dice.test_should_compare_two_pair_of_dices()
+	-- given
+	local attack_dice_1 = dice.new(3)
+	local attack_dice_2 = dice.new(6)
+	local defense_dice_1 = dice.new(5)
+	local defense_dice_2 = dice.new(4)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, attack_dice_2, nil, defense_dice_1, defense_dice_2, nil)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 1)
+	lu.assertEquals(result.defense_victories, 1)
+end
+
+function test_dice.test_should_compare_3_attack_dices_against_1_defense()
+	-- given
+	local attack_dice_1 = dice.new(3)
+	local attack_dice_2 = dice.new(6)
+	local attack_dice_3 = dice.new(1)
+	local defense_dice_1 = dice.new(5)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, attack_dice_2, attack_dice_3, defense_dice_1, nil, nil)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 1)
+	lu.assertEquals(result.defense_victories, 0)
+end
+
+function test_dice.test_should_compare_3_attack_dices_against_1_defense_where_the_defense_wins()
+	-- given
+	local attack_dice_1 = dice.new(3)
+	local attack_dice_2 = dice.new(4)
+	local attack_dice_3 = dice.new(1)
+	local defense_dice_1 = dice.new(5)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, attack_dice_2, attack_dice_3, defense_dice_1, nil, nil)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 0)
+	lu.assertEquals(result.defense_victories, 1)
+end
+
+function test_dice.test_should_compare_1_attack_dices_against_3_defense()
+	-- given
+	local attack_dice_1 = dice.new(3)
+	local defense_dice_1 = dice.new(5)
+	local defense_dice_2 = dice.new(6)
+	local defense_dice_3 = dice.new(1)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, nil, nil, defense_dice_1, defense_dice_2, defense_dice_3)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 0)
+	lu.assertEquals(result.defense_victories, 1)
+end
+
+function test_dice.test_should_compare_1_attack_dices_against_3_defense_where_the_attack_wins()
+	-- given
+	local attack_dice_1 = dice.new(6)
+	local defense_dice_1 = dice.new(5)
+	local defense_dice_2 = dice.new(4)
+	local defense_dice_3 = dice.new(1)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, nil, nil, defense_dice_1, defense_dice_2, defense_dice_3)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 1)
+	lu.assertEquals(result.defense_victories, 0)
+end
+
+function test_dice.test_should_defense_should_win_when_comparing_draws()
+	-- given
+	local attack_dice_1 = dice.new(6)
+	local attack_dice_2 = dice.new(6)
+	local attack_dice_3 = dice.new(3)
+	local defense_dice_1 = dice.new(6)
+	local defense_dice_2 = dice.new(6)
+	local defense_dice_3 = dice.new(1)
+
+	-- when:
+	local result = dice.compare(attack_dice_1, attack_dice_2, attack_dice_3, defense_dice_1, defense_dice_2, defense_dice_3)
+
+	-- then:
+	lu.assertEquals(result.attack_victories, 1)
+	lu.assertEquals(result.defense_victories, 2)
+end
+
+
